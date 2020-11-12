745b33319d0990e3f4be6a79c121d2503f7349df
pvphero <impvphero@163.com>
2020-11-12 11:59:21 +0800

    Day1112

--- src/com/vv/Day1112Fibonacci.java
+++ src/com/vv/Day1112Fibonacci.java
@@ -10,20 +10,20 @@ class Day1112Fibonacci {
 
     }
 
-    public static int fib(int n){
-        if (n==0){
+    public static int fib(int n) {
+        if (n == 0) {
             return 0;
         }
-        if (n==1){
+        if (n == 1) {
             return 1;
         }
-        int min1=0;
-        int min2=1;
-        int result=0;
-        for (int i=2;i<=n;i++){
-            result=(min1+min2)%1000000007;
-            min1=min2;
-            min2=result;
+        int min1 = 0;
+        int min2 = 1;
+        int result = 0;
+        for (int i = 2; i <= n; i++) {
+            result = (min1 + min2) % 1000000007;
+            min1 = min2;
+            min2 = result;
         }
         return result;
     }
