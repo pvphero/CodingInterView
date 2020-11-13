b60588542fad1f26fd63fa79282c701af51a378e
pvphero <impvphero@163.com>
2020-11-13 09:11:21 +0800

    Day1113

--- src/com/vv/Day1113FibSteps.java
+++ src/com/vv/Day1113FibSteps.java
@@ -1,6 +1,17 @@
 package com.vv;
 
 /**
+ * 剑指 Offer 10- II. 青蛙跳台阶问题
+ * 一只青蛙一次可以跳上1级台阶，也可以跳上2级台阶。求该青蛙跳上一个 n 级的台阶总共有多少种跳法。
+ * <p>
+ * 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/qing-wa-tiao-tai-jie-wen-ti-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
  * @author ShenZhenWei
  * @date 2020/11/13
  */
