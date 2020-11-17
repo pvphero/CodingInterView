f0bcc6e350941242724d970fef3447afb30c57bf
pvphero <impvphero@163.com>
2020-11-17 10:20:42 +0800

    Day1117

---
+++ src/com/vv/Day1117ReverseList.java
@@ -0,0 +1,45 @@
+package com.vv;
+
+import com.vv.data.ListNode;
+
+/**
+ * 剑指 Offer 24. 反转链表
+ * 定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+ * <p>
+ *  
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author ShenZhenWei
+ * @date 2020/11/17
+ */
+class Day1117ReverseList {
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(3);
+        ListNode listNode3 = new ListNode(2);
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        System.out.println(reverseListNode(listNode1).value + "");
+    }
+
+    public static ListNode reverseListNode(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        ListNode tmp = null;
+        while (cur != null) {
+            tmp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
+    }
+}
