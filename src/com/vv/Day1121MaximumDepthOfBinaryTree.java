eeb0156e1109463aea426d32f0b3c71c9706ab8d
pvphero <impvphero@163.com>
2020-11-21 22:45:26 +0800

    Day1121

---
+++ src/com/vv/Day1121MaximumDepthOfBinaryTree.java
@@ -0,0 +1,46 @@
+package com.vv;
+
+import com.vv.data.TreeNode;
+
+/**
+ * 104. 二叉树的最大深度
+ * <p>
+ * 给定一个二叉树，找出其最大深度。
+ * <p>
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ * <p>
+ * 说明: 叶子节点是指没有子节点的节点。
+ * <p>
+ * 示例：
+ * 给定二叉树 [3,9,20,null,null,15,7]，
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 返回它的最大深度 3 。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximum-depth-of-binary-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author ShenZhenWei
+ * @date 2020/11/21
+ */
+public class Day1121MaximumDepthOfBinaryTree {
+    public static void main(String[] args) {
+
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int leftHeight = maxDepth(root.left);
+            int rightHeight = maxDepth(root.right);
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+
+}
