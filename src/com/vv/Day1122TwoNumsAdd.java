8f9a1ad1a7f3e842fd197d54ebdde85864ad0599
pvphero <impvphero@163.com>
2020-11-22 23:36:10 +0800

    Day1122

---
+++ src/com/vv/Day1122TwoNumsAdd.java
@@ -0,0 +1,55 @@
+package com.vv;
+
+import com.vv.data.ListNode;
+
+/**
+ * 2. 两数相加
+ * <p>
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ * <p>
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ * <p>
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/add-two-numbers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author ShenZhenWei
+ * @date 2020/11/22
+ */
+public class Day1122TwoNumsAdd {
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) {
+            return null;
+        }
+        ListNode dumry = new ListNode(0);
+        ListNode head = dumry;
+        int addOne = 0;
+        while (l1 != null || l2 != null || addOne != 0) {
+            int val1 = l1 != null ? l1.value : 0;
+            int val2 = l2 != null ? l2.value : 0;
+            int sum = val1 + val2 + addOne;
+            head.next = new ListNode(sum % 10);
+            addOne = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        return dumry.next;
+    }
+
+}
