7deb323ba58a84d653da2cb8d6a08734d12224d3
pvphero <impvphero@163.com>
2020-12-02 18:03:54 +0800

    Day1202 三数相加 模板解题

--- src/com/vv/Day1112QueueWithTwoStacks.java
+++ src/com/vv/Day1112QueueWithTwoStacks.java
@@ -43,14 +43,58 @@ public class Day1112QueueWithTwoStacks {
         }
 
         public int deleteHead() {
-            if (stack2.isEmpty()){
-                while (!stack1.isEmpty()){
+            if (stack2.isEmpty()) {
+                while (!stack1.isEmpty()) {
                     stack2.push(stack1.pop());
                 }
             }
-            return stack2.isEmpty()?-1:stack2.pop();
+            return stack2.isEmpty() ? -1 : stack2.pop();
         }
     }
+
+
+//    vector<vector<int>> twoSumTarget(vector<int>& nums,int start, int target) {
+//        // nums 数组必须有序
+//        sort(nums.begin(), nums.end());
+//        int lo = start, hi = nums.size() - 1;
+//        vector<vector<int>> res;
+//        while (lo < hi) {
+//            int sum = nums[lo] + nums[hi];
+//            int left = nums[lo], right = nums[hi];
+//            if (sum < target) {
+//                while (lo < hi && nums[lo] == left) lo++;
+//            } else if (sum > target) {
+//                while (lo < hi && nums[hi] == right) hi--;
+//            } else {
+//                res.push_back({left, right});
+//                while (lo < hi && nums[lo] == left) lo++;
+//                while (lo < hi && nums[hi] == right) hi--;
+//            }
+//        }
+//        return res;
+//    }
+//
+//    /* 计算数组 nums 中所有和为 target 的三元组 */
+//    vector<vector<int>> threeSumTarget(vector<int>& nums, int target) {
+//        // 数组得排个序
+//        sort(nums.begin(), nums.end());
+//        int n = nums.size();
+//        vector<vector<int>> res;
+//        // 穷举 threeSum 的第一个数
+//        for (int i = 0; i < n; i++) {
+//            // 对 target - nums[i] 计算 twoSum
+//            vector<vector<int>>
+//                    tuples = twoSumTarget(nums, i + 1, target - nums[i]);
+//            // 如果存在满足条件的二元组，再加上 nums[i] 就是结果三元组
+//            for (vector<int>& tuple : tuples) {
+//                tuple.push_back(nums[i]);
+//                res.push_back(tuple);
+//            }
+//            // 跳过第一个数字重复的情况，否则会出现重复结果
+//            while (i < n - 1 && nums[i] == nums[i + 1]) i++;
+//        }
+//        return res;
+//    }
 
 }
 
---
+++ src/com/vv/Day12013sum.java
@@ -0,0 +1,112 @@
+package com.vv;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 字节题库
+ * 15. 三数之和
+ * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+ * <p>
+ * 注意：答案中不可以包含重复的三元组。
+ * <p>
+ *  
+ * <p>
+ * 示例：
+ * <p>
+ * 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+ * <p>
+ * 满足要求的三元组集合为：
+ * [
+ * [-1, 0, 1],
+ * [-1, -1, 2]
+ * ]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/3sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author ShenZhenWei
+ * @date 2020/12/1
+ */
+class Day12013sum {
+    public static void main(String[] args) {
+        int[] a = new int[]{-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> list = threeSumWithTarget(a, 0);
+        for (int i = 0; i < list.size(); i++) {
+            for (int k = 0; k < list.get(i).size(); k++) {
+                System.out.print(list.get(i).get(k));
+            }
+            System.out.println();
+        }
+    }
+
+    public static List<List<Integer>> twoSum(int[] nums, int start, int target) {
+        //1.对数组排序
+        int[] tmpl = new int[nums.length];
+        System.arraycopy(nums, 0, tmpl, 0, nums.length);
+        Arrays.sort(nums);
+        //2.定义两个指针
+        int low = start;
+        int heigh = nums.length - 1;
+        int sum = 0;
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        List<Integer> list = new ArrayList<Integer>();
+        while (low < heigh) {
+            sum = nums[low] + nums[heigh];
+            //3.定义 left right保存nums[low] nums[heigh]的值
+            int left = nums[low];
+            int right = nums[heigh];
+            if (sum < target) {
+                while (low < heigh && nums[low] == left) {
+                    low++;
+                }
+            } else if (sum > target) {
+                while (low < heigh && nums[heigh] == right) {
+                    heigh--;
+                }
+            } else {
+                list.add(left);
+                list.add(right);
+                result.add(list);
+                while (low < heigh && nums[low] == left) {
+                    low++;
+                }
+                while (low < heigh && nums[heigh] == right) {
+                    heigh--;
+                }
+            }
+        }
+
+        return result;
+
+
+    }
+
+    public static List<List<Integer>> threeSumWithTarget(int[] nums, int target) {
+        //1.排序
+        int[] tmpl = new int[nums.length];
+        System.arraycopy(nums, 0, tmpl, 0, nums.length);
+        Arrays.sort(nums);
+        int n = nums.length;
+        //穷举
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        for (int i = 0; i < n; i++) {
+            //对 target-num[i] 计算twoSum
+            List<List<Integer>> list = twoSum(nums, i + 1, target - nums[i]);
+            for (List<Integer> data : list) {
+                data.add(nums[i]);
+                res.add(data);
+            }
+
+            //跳出第一个数字重复的情况
+            while (i < n - 1 && nums[i] == nums[i + 1]) {
+                i++;
+            }
+        }
+        return res;
+    }
+
+
+}
