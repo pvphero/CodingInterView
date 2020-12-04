b7c62fcc066b04cd42382da95e334139dc4270cf
pvphero <impvphero@163.com>
2020-12-04 23:24:14 +0800

    Day1204

---
+++ src/com/vv/Day1204KList.java
@@ -0,0 +1,81 @@
+package com.vv;
+
+import com.vv.data.ListNode;
+
+/**
+ * 字节题库
+ * 25. K 个一组翻转链表
+ * 给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
+ * <p>
+ * k 是一个正整数，它的值小于或等于链表的长度。
+ * <p>
+ * 如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+ * <p>
+ *  
+ * <p>
+ * 示例：
+ * <p>
+ * 给你这个链表：1->2->3->4->5
+ * <p>
+ * 当 k = 2 时，应当返回: 2->1->4->3->5
+ * <p>
+ * 当 k = 3 时，应当返回: 3->2->1->4->5
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reverse-nodes-in-k-group
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author ShenZhenWei
+ * @date 2020/12/4
+ */
+class Day1204KList {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(3);
+        l1.next.next.next = new ListNode(4);
+        System.out.println(reverseKGroup(l1, 2));
+    }
+
+    public static ListNode reverseKGroup(ListNode head, int k) {
+        ListNode hair = new ListNode(0);
+        hair.next = head;
+        ListNode pre = hair;
+
+        while (head != null) {
+            ListNode tail = pre;
+            // 查看剩余部分长度是否大于等于 k
+            for (int i = 0; i < k; ++i) {
+                tail = tail.next;
+                if (tail == null) {
+                    return hair.next;
+                }
+            }
+            ListNode nex = tail.next;
+            ListNode[] reverse = myReverse(head, tail);
+            head = reverse[0];
+            tail = reverse[1];
+            // 把子链表重新接回原链表
+            pre.next = head;
+            tail.next = nex;
+            pre = tail;
+            head = tail.next;
+        }
+
+        return hair.next;
+    }
+
+    public static ListNode[] myReverse(ListNode head, ListNode tail) {
+        ListNode prev = tail.next;
+        ListNode p = head;
+        while (prev != tail) {
+            ListNode nex = p.next;
+            p.next = prev;
+            prev = p;
+            p = nex;
+        }
+        return new ListNode[]{tail, head};
+    }
+
+
+}
