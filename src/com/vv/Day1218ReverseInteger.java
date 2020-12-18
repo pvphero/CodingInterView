a08ed97f3f5846fff7b1ab4f50e1aa2bf8f0c566
pvphero <impvphero@163.com>
2020-12-18 23:15:31 +0800

    Day1218

--- src/com/vv/Day1218ReverseInteger.java
+++ src/com/vv/Day1218ReverseInteger.java
@@ -26,17 +26,33 @@ package com.vv;
  * 链接：https://leetcode-cn.com/problems/reverse-integer
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  *
+ * 题解：https://leetcode-cn.com/problems/reverse-integer/solution/tu-jie-7-zheng-shu-fan-zhuan-by-wang_ni_ma/
+ *
  * @author ShenZhenWei
  * @date 2020/12/18
  */
 public class Day1218ReverseInteger {
     public static void main(String[] args) {
-
+        Solution solution = new Solution();
+        System.out.println(solution.reverse(12337));
     }
 
     static class Solution {
         public int reverse(int x) {
-
+            int res = 0;
+            while (x != 0) {
+                int tmp = x % 10;
+                //判断是否 大于最大整数
+                if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && tmp > 7)) {
+                    return 0;
+                }
+                if (res < Integer.MIN_VALUE / 10 || (res == Integer.MIN_VALUE / 10 && tmp < -8)) {
+                    return 0;
+                }
+                res = res * 10 + tmp;
+                x = x / 10;
+            }
+            return res;
         }
     }
 }
