8df766ad77882d2d04563894a2f03b6f66672488
pvphero <impvphero@163.com>
2020-12-19 13:32:49 +0800

    Day1219

---
+++ src/com/vv/Day1219KthLargestElementInAnArray.java
@@ -0,0 +1,42 @@
+package com.vv;
+
+import java.util.Arrays;
+
+/**
+ * 字节题库
+ * 215. 数组中的第K个最大元素
+ * <p>
+ * 在未排序的数组中找到第 k 个最大的元素。请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [3,2,1,5,6,4] 和 k = 2
+ * 输出: 5
+ * 示例 2:
+ * <p>
+ * 输入: [3,2,3,1,2,4,5,5,6] 和 k = 4
+ * 输出: 4
+ * 说明:
+ * <p>
+ * 你可以假设 k 总是有效的，且 1 ≤ k ≤ 数组的长度。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/kth-largest-element-in-an-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author ShenZhenWei
+ * @date 2020/12/19
+ */
+public class Day1219KthLargestElementInAnArray {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.findKthLargest(new int[]{1, 2, 3, 41, 21,}, 3));
+    }
+
+    static class Solution {
+        public int findKthLargest(int[] nums, int k) {
+            Arrays.sort(nums);
+            return nums[nums.length - k];
+        }
+    }
+}
