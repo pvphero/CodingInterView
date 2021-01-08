79b190cbdb24886db61a2f9fea77eb662c78b6df
pvphero <impvphero@163.com>
2021-01-08 11:13:37 +0800

    Day20210108

---
+++ src/com/vv/Day20210108LinkedListCycleIi.java
@@ -0,0 +1,71 @@
+package com.vv;
+
+import com.vv.data.ListNode;
+
+/**
+ * hot100
+ * 142. 环形链表 II
+ * <p>
+ * 给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+ * <p>
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。注意，pos 仅仅是用于标识环的情况，并不会作为参数传递到函数中。
+ * <p>
+ * 说明：不允许修改给定的链表。
+ * <p>
+ * 进阶：
+ * <p>
+ * 你是否可以使用 O(1) 空间解决此题？
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：返回索引为 1 的链表节点
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/linked-list-cycle-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author ShenZhenWei
+ * @date 2021/1/8
+ */
+public class Day20210108LinkedListCycleIi {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Definition for singly-linked list.
+     * class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) {
+     * val = x;
+     * next = null;
+     * }
+     * }
+     */
+    public class Solution {
+        public ListNode detectCycle(ListNode head) {
+            ListNode fast = head;
+            ListNode slow = head;
+
+            while (fast != null && fast.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+                if (fast == slow) {
+                    ListNode p = head;
+                    while (p != slow) {
+                        p = p.next;
+                        slow = slow.next;
+                    }
+                    return slow;
+                }
+            }
+            return null;
+        }
+    }
+}
