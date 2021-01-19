322e1c29b7dc5735709329e9a9224a3f40071866
pvphero <impvphero@163.com>
2021-01-19 11:34:09 +0800

    Day20210119

---
+++ src/com/vv/Day20210119NextPermutation.java
@@ -0,0 +1,79 @@
+package com.vv;
+
+/**
+ * 字节题库
+ * 31. 下一个排列
+ * <p>
+ * 实现获取 下一个排列 的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
+ * <p>
+ * 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+ * <p>
+ * 必须 原地 修改，只允许使用额外常数空间。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,2,3]
+ * 输出：[1,3,2]
+ * 示例 2：
+ * <p>
+ * 输入：nums = [3,2,1]
+ * 输出：[1,2,3]
+ * 示例 3：
+ * <p>
+ * 输入：nums = [1,1,5]
+ * 输出：[1,5,1]
+ * 示例 4：
+ * <p>
+ * 输入：nums = [1]
+ * 输出：[1]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/next-permutation
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author ShenZhenWei
+ * @date 2021/1/19
+ */
+public class Day20210119NextPermutation {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.nextPermutation(new int[]{1, 2, 3});
+    }
+
+    static class Solution {
+        public void nextPermutation(int[] nums) {
+            int i = nums.length - 2;
+            while (i >= 0 && nums[i] >= nums[i + 1]) {
+                i--;
+            }
+
+            if (i >= 0) {
+                int j = nums.length - 1;
+                while (j > 0 && nums[i] >= nums[j]) {
+                    j--;
+                }
+                swap(nums, i, j);
+            }
+            reverse(nums, i + 1);
+
+        }
+
+        public void swap(int[] nums, int i, int j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+
+        public void reverse(int[] nums, int start) {
+            int left = start, right = nums.length - 1;
+            while (left < right) {
+                swap(nums, left, right);
+                left++;
+                right--;
+            }
+        }
+
+    }
+}
